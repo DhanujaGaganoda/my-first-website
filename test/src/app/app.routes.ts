6787ed63db1f49c55e942c8557bc644e07f8a571
DhanujaGaganoda <dhanujag.bg2023a@gmail.com>
2023-12-06 14:21:06 +0530

    app update

--- test/src/app/app.routes.ts
+++ test/src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
+import { ProfileComponent } from './profile/profile.component';
 
 export const routes: Routes = [
     { path: '', component: HomeComponent },
-    { path: 'Login', component: LoginComponent }
+    { path: 'Login', component: LoginComponent },
+    { path: 'profile', component: ProfileComponent }
 ];
--- test/src/app/login/login.component.ts
+++ test/src/app/login/login.component.ts
@@ -12,3 +12,4 @@ export class LoginComponent {
   p="Login Page";
 
 }
+
